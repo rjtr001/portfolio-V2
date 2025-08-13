27253e77cdde827d83cdf1be3899bf5ec9e61934
Thanaraman RJ <107317555+rjtr001@users.noreply.github.com>
2025-08-13 23:50:41 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/portfolio-V2/", // <-- ADD THIS LINE
   plugins: [react()],
   resolve: {
     alias: {
